b5a87e475fd17ad7ac0a300b4f794f1b8bf8c29f
Wojtron <wojciechwrobel1994@gmail.com>
2023-10-09 11:36:41 -0600

    Second Commit. Added 3 more companies to scrap

--- index.js
+++ index.js
@@ -4,13 +4,83 @@ const express = require('express')
 const {response} = require("express");
 const PORT = 8000
 
-const url = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=KERNEL'
+const KERNEL = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=KERNEL'
+const MOSTAL = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=MOSTALPLC'
+const CDPROJEKT = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=CDPROJEKT'
+const JR = 'https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=JRHOLDING'
+const app = express()
 
-axios(url)
+
+axios(KERNEL)
     .then(response=>{
         const html = response.data
-        console.log(html)
-    })
+        const $ = cheerio.load(html)
+        const articles =[]
 
-const app = express()
+        $('.summaryTable',html).each(function (){
+           const title= $(this).text().trim()
+           const link = $(this).find('tr').attr("class")
+           articles.push({
+               title,
+               link
+           })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+
+axios(MOSTAL)
+    .then(response=>{
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles =[]
+
+        $('.summaryTable',html).each(function (){
+            const title= $(this).text().trim()
+            const link = $(this).find('tr').attr("class")
+            articles.push({
+                title,
+                link
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+
+axios(CDPROJEKT)
+    .then(response=>{
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles =[]
+
+        $('.summaryTable',html).each(function (){
+            const title= $(this).text().trim()
+            const link = $(this).find('tr').attr("class")
+            articles.push({
+                title,
+                link
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+
+axios(JR)
+    .then(response=>{
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles =[]
+
+        $('.summaryTable',html).each(function (){
+            const title= $(this).text().trim()
+            const link = $(this).find('tr').attr("class")
+            articles.push({
+                title,
+                link
+            })
+        })
+        console.log(articles)
+    }).catch(err => console.log(err))
+
+
 app.listen(PORT, () =>console.log(`server running on PORT ${PORT}`))
